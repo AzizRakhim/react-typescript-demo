2d5211142fcd0e48ee73eaa7fde65dd5c43eb0ba
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-08-09 08:32:39 +0500

    lesson approx 11. About useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button";
+import Container from "./components/Container";
+import Greet from "./components/Greet";
+import Heading from "./components/Heading";
+import Input from "./components/Input";
+import Oscar from "./components/Oscar";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
+import Status from "./components/Status";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
+  const nameList = [
+    { first: "Bruce", last: "Wayne" },
+    { first: "Clark", last: "Kent" },
+    { first: "Princess", last: "Diana" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Wishwas" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
+      <Status status="error" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
+      <Greet name="Wishwas" messageCount={10} isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
---
+++ src/components/state/User.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+type AuthUser = {
+  name: string;
+  email: string;
+};
+
+function User() {
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
+
+  const handleLogin = () => {
+    setUser({
+      name: "Wishwas",
+      email: "wishwas@example.com",
+    });
+  };
+
+  // const handleLogout = () => {
+  //   setUser(null);
+  // };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+
+      {/* <button onClick={handleLogout}>Logout</button> */}
+
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
+    </div>
+  );
+}
+
+export default User;
